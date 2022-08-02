5313e21cd6f0c01dae2e3783b5cfed6c00892f9e
Beanxx <tnqls991211@naver.com>
2022-08-02 22:20:02 +0900

    feat: React에 GraphQL 적용하기

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
+import { graphql } from "@octokit/graphql"; // github server에 접근하기 편하도록
+import { useEffect, useState } from "react";
+import Discussions from "./Discussions";
+
+const TOKEN_ID = process.env.REACT_APP_TOKEN_ID;
+
+async function getRepository() {
+  const { repository } = await graphql(
+    `
+      {
+        repository(name: "agora-states-fe", owner: "codestates-seb") {
+          discussions(first: 100) {
+            edges {
+              node {
+                id
+                title
+                createdAt
+                url
+                author {
+                  login
+                  avatarUrl
+                }
+                category {
+                  name
+                }
+                answer {
+                  author {
+                    login
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    `,
+    {
+      headers: {
+        authorization: `token ${TOKEN_ID}`,
+      },
+    }
+  );
+
+  return repository;
+}
 
 function App() {
+  const [repository, setRepository] = useState({});
+  const { discussions } = repository;
+
+  useEffect(() => {
+    getRepository()
+      .then((data) => {
+        setRepository(data);
+      })
+      .catch((error) => {
+        console.log(Error, error);
+      });
+  }, []);
+
+  console.log(discussions);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main">
+      <header>
+        <h1>My Agora States</h1>
       </header>
+      <div className="main-wrapper">
+        {discussions !== undefined ? (
+          <Discussions discussions={discussions} />
+        ) : (
+          <div>loading...</div>
+        )}
+      </div>
+      <footer>codestates</footer>
     </div>
   );
 }
