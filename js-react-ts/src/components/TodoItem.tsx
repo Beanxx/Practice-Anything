0de367d3da578d09ac41da80a4adb9c22fc84973
Beanxx <tnqls991211@naver.com>
2022-11-03 21:36:06 +0900

    feat: TS로 간단Todo List 구현

---
+++ js-react-ts/src/components/TodoItem.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const TodoItem: React.FC<{
+  text: string;
+  onRemoveTodo: (event: React.MouseEvent) => void;
+}> = (props) => {
+  return <li onClick={props.onRemoveTodo}>{props.text}</li>;
+};
+
+export default TodoItem;
