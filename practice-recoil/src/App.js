1efffb650f4d2f422183fda75f69cbc4be6406f3
Beanxx <tnqls991211@naver.com>
2023-02-08 17:49:29 +0900

    feat: RecoilRoot 설정

--- practice-recoil/src/App.js
+++ practice-recoil/src/App.js
@@ -1,8 +1,16 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import CharacterCounter from "./CharacterCounter";
+import { RecoilRoot } from "recoil";
 
 function App() {
-  return <div>Practice-Recoil</div>;
+  return (
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
 }
 
 export default App;
+
+// Recoil를 사용하는 컴포넌트 즉 부모 컴포넌트에는 RecoilRoot가 필요!!
+// RecoilRoot는 루트 컴포넌트에 넣는 것이 Good!
