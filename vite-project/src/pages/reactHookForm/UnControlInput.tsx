2ef642a5e1d40d98cf22c603d5fd8a800c5328d4
Beanxx <tnqls991211@naver.com>
2023-08-22 12:28:28 +0900

    feat: add unControlled input (register)

---
+++ vite-project/src/pages/reactHookForm/UnControlInput.tsx
@@ -0,0 +1,21 @@
+import { FieldPath, FieldValues, UseFormRegister } from "react-hook-form";
+
+type TControl<T extends FieldValues> = {
+  name: FieldPath<T>;
+  register: UseFormRegister<T>;
+};
+
+// 비제어 Input
+const UnControlInput = <T extends FieldValues>({
+  name,
+  register,
+}: TControl<T>) => {
+  return (
+    <div style={{ display: "flex", gap: "10px" }}>
+      <label htmlFor={name}>{name}</label>
+      <input type="text" {...register(name, { required: true })} />
+    </div>
+  );
+};
+
+export default UnControlInput;
